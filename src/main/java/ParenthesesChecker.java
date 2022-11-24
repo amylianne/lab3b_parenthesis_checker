c6b73a8ebc4061a1e6e260fbd5f0f9a5c8081c53
amylianne <amywynn.3.a@gmail.com>
2022-11-24 14:34:00 +0000

    start code

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,13 @@
+public class ParenthesesChecker {
+
+    //write a method called checkParentheses()
+//    takes in a String
+//    returns a boolean -true
+
+    //return true is the string contains "correct" pairs of parentheses
+    //return false if not
+    // false situations:
+        //more opening or closing brackets of particular type () <> {} []
+        //closing bracket followed by opening one
+        //opening bracket followed by 2 closing brackets
+}
